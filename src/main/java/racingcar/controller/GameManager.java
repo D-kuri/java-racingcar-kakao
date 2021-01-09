0b73cb60db86e9a50cf6e4e907db84c4b72d3cc7
kuri <denver5636@naver.com>
2021-01-09 15:27:01 +0900

    [refactor] view에서 domain으로의 의존관계 설정후 로직 처리

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -1,13 +1,10 @@
 package racingcar.controller;
 
-import racingcar.domain.Car;
 import racingcar.domain.Cars;
 import racingcar.domain.RandomNumberMoveStrategy;
 import racingcar.domain.Round;
 import racingcar.view.InputView;
 import racingcar.view.ResultView;
-
-import java.util.stream.Collectors;
 
 public class GameManager {
     private static Cars cars;
@@ -17,7 +14,7 @@ public class GameManager {
         Round round = new Round(InputView.inputRound());
 
         startRound(round);
-        ResultView.printWinner(getWinner());
+        ResultView.printWinner(cars);
     }
 
     private static void startRound(Round round) {
@@ -25,32 +22,8 @@ public class GameManager {
 
         for (int i = 0; i < round.getRound(); i++) {
             cars.move(new RandomNumberMoveStrategy());
-            ResultView.print(roundResult());
+            ResultView.printRoundResult(cars);
         }
-    }
-
-    private static String roundResult() {
-        String roundResult = "";
-        for (Car car : cars.getCars()) {
-            roundResult += car.getName()
-                    + " : "
-                    + new String(new char[car.getLocation()]).replace("\0", "_")
-                    + "\n";
-        }
-        return roundResult;
-    }
-
-    private static String getWinner() {
-        return cars.getCars().stream()
-                .filter(car -> car.getLocation() == getWinnerLocation())
-                .map(Car::getName)
-                .collect(Collectors.joining(" "));
-    }
-
-    private static int getWinnerLocation() {
-        return cars.getCars().stream()
-                .mapToInt(car -> car.getLocation())
-                .max().orElse(0);
     }
 
 }
--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -1,5 +1,10 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+import racingcar.domain.Cars;
+
+import java.util.stream.Collectors;
+
 public class ResultView {
     private static final String ROUND_START = "실행 결과";
     private static final String GAME_RESULT = "가 최종 우승했습니다";
@@ -8,12 +13,27 @@ public class ResultView {
         System.out.println("\n" + ROUND_START);
     }
 
-    public static void print(String str) {
-        System.out.println(str);
+    public static void printRoundResult(Cars cars) {
+        String roundResult = "";
+        for (Car car : cars.getCars()) {
+            roundResult += car.getName()
+                    + " : "
+                    + new String(new char[car.getLocation()]).replace("\0", "_")
+                    + "\n";
+        }
+
+        System.out.println(roundResult);
     }
 
-    public static void printWinner(String winner) {
-        System.out.println(winner + GAME_RESULT);
+    public static void printWinner(Cars cars) {
+        int winnerLocation = cars.getCars().stream()
+                .mapToInt(car -> car.getLocation())
+                .max().orElse(0);
+        System.out.println(cars.getCars().stream()
+                .filter(car -> car.getLocation() == winnerLocation)
+                .map(Car::getName)
+                .collect(Collectors.joining(" "))
+                + GAME_RESULT);
     }
 
 }
